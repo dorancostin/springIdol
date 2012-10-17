b28427999a2defdce93fffcbbfe1fc215dd062d0
costin <doran_costin@yahoo.com>
2012-10-17 20:19:31 +0300

    first ver

---
+++ src/main/java/com/dummy/Hell.java
@@ -0,0 +1,14 @@
+package com.dummy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: costin
+ * Date: 10/17/12
+ * Time: 8:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hell {
+     public static void main(String[] args) {
+         System.out.println("Welcome to hell, ladies");
+     }
+}
